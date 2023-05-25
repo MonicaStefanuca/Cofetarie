59dc8969d67b57034fa0270cd624b4035fc11cd4
Monica Ștefănucă <monica.stefanuca1@student.usv.ro>
2023-05-26 01:14:39 +0300

    Check Box added

--- InterfataUtilizator_WindowsForms/Form2.Designer.cs
+++ InterfataUtilizator_WindowsForms/Form2.Designer.cs
@@ -43,13 +43,16 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.NumarProdusePreparate = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.pretBox = new System.Windows.Forms.CheckBox();
+            this.gramajBox = new System.Windows.Forms.CheckBox();
             this.SuspendLayout();
             // 
             // NumeProdus
             // 
-            this.NumeProdus.Location = new System.Drawing.Point(110, 86);
+            this.NumeProdus.Location = new System.Drawing.Point(147, 106);
+            this.NumeProdus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.NumeProdus.Name = "NumeProdus";
-            this.NumeProdus.Size = new System.Drawing.Size(187, 20);
+            this.NumeProdus.Size = new System.Drawing.Size(248, 22);
             this.NumeProdus.TabIndex = 0;
             // 
             // Nume
@@ -57,9 +60,10 @@
             this.Nume.AutoSize = true;
             this.Nume.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Nume.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.Nume.Location = new System.Drawing.Point(12, 84);
+            this.Nume.Location = new System.Drawing.Point(16, 103);
+            this.Nume.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Nume.Name = "Nume";
-            this.Nume.Size = new System.Drawing.Size(51, 20);
+            this.Nume.Size = new System.Drawing.Size(64, 25);
             this.Nume.TabIndex = 1;
             this.Nume.Text = "Nume";
             this.Nume.Click += new System.EventHandler(this.label1_Click);
@@ -69,9 +73,10 @@
             this.Pret.AutoSize = true;
             this.Pret.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Pret.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.Pret.Location = new System.Drawing.Point(12, 205);
+            this.Pret.Location = new System.Drawing.Point(17, 224);
+            this.Pret.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Pret.Name = "Pret";
-            this.Pret.Size = new System.Drawing.Size(38, 20);
+            this.Pret.Size = new System.Drawing.Size(47, 25);
             this.Pret.TabIndex = 2;
             this.Pret.Text = "Pret";
             this.Pret.Click += new System.EventHandler(this.label2_Click);
@@ -81,9 +86,10 @@
             this.Gramaj.AutoSize = true;
             this.Gramaj.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Gramaj.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.Gramaj.Location = new System.Drawing.Point(12, 245);
+            this.Gramaj.Location = new System.Drawing.Point(17, 269);
+            this.Gramaj.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Gramaj.Name = "Gramaj";
-            this.Gramaj.Size = new System.Drawing.Size(61, 20);
+            this.Gramaj.Size = new System.Drawing.Size(75, 25);
             this.Gramaj.TabIndex = 3;
             this.Gramaj.Text = "Gramaj";
             this.Gramaj.Click += new System.EventHandler(this.label3_Click);
@@ -93,9 +99,10 @@
             this.Descriere.AutoSize = true;
             this.Descriere.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Descriere.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.Descriere.Location = new System.Drawing.Point(12, 167);
+            this.Descriere.Location = new System.Drawing.Point(16, 187);
+            this.Descriere.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.Descriere.Name = "Descriere";
-            this.Descriere.Size = new System.Drawing.Size(77, 20);
+            this.Descriere.Size = new System.Drawing.Size(95, 25);
             this.Descriere.TabIndex = 4;
             this.Descriere.Text = "Descriere";
             // 
@@ -104,38 +111,43 @@
             this.id.AutoSize = true;
             this.id.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.id.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.id.Location = new System.Drawing.Point(12, 128);
+            this.id.Location = new System.Drawing.Point(16, 145);
+            this.id.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.id.Name = "id";
-            this.id.Size = new System.Drawing.Size(23, 20);
+            this.id.Size = new System.Drawing.Size(28, 25);
             this.id.TabIndex = 5;
             this.id.Text = "Id";
             // 
             // idProdus
             // 
-            this.idProdus.Location = new System.Drawing.Point(110, 128);
+            this.idProdus.Location = new System.Drawing.Point(147, 145);
+            this.idProdus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.idProdus.Name = "idProdus";
-            this.idProdus.Size = new System.Drawing.Size(187, 20);
+            this.idProdus.Size = new System.Drawing.Size(248, 22);
             this.idProdus.TabIndex = 6;
             // 
             // descriereProdus
             // 
-            this.descriereProdus.Location = new System.Drawing.Point(110, 167);
+            this.descriereProdus.Location = new System.Drawing.Point(147, 187);
+            this.descriereProdus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.descriereProdus.Name = "descriereProdus";
-            this.descriereProdus.Size = new System.Drawing.Size(187, 20);
+            this.descriereProdus.Size = new System.Drawing.Size(248, 22);
             this.descriereProdus.TabIndex = 7;
             // 
             // pretProdus
             // 
-            this.pretProdus.Location = new System.Drawing.Point(110, 205);
+            this.pretProdus.Location = new System.Drawing.Point(147, 227);
+            this.pretProdus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pretProdus.Name = "pretProdus";
-            this.pretProdus.Size = new System.Drawing.Size(187, 20);
+            this.pretProdus.Size = new System.Drawing.Size(248, 22);
             this.pretProdus.TabIndex = 8;
             // 
             // gramajProdus
             // 
-            this.gramajProdus.Location = new System.Drawing.Point(110, 245);
+            this.gramajProdus.Location = new System.Drawing.Point(147, 269);
+            this.gramajProdus.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.gramajProdus.Name = "gramajProdus";
-            this.gramajProdus.Size = new System.Drawing.Size(187, 20);
+            this.gramajProdus.Size = new System.Drawing.Size(248, 22);
             this.gramajProdus.TabIndex = 9;
             // 
             // label6
@@ -143,9 +155,10 @@
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("MV Boli", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label6.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.label6.Location = new System.Drawing.Point(32, 23);
+            this.label6.Location = new System.Drawing.Point(43, 28);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(265, 41);
+            this.label6.Size = new System.Drawing.Size(330, 52);
             this.label6.TabIndex = 10;
             this.label6.Text = "Adaugare produs";
             // 
@@ -153,18 +166,20 @@
             // 
             this.adauga.BackColor = System.Drawing.Color.SaddleBrown;
             this.adauga.ForeColor = System.Drawing.Color.PeachPuff;
-            this.adauga.Location = new System.Drawing.Point(120, 332);
+            this.adauga.Location = new System.Drawing.Point(161, 447);
+            this.adauga.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.adauga.Name = "adauga";
-            this.adauga.Size = new System.Drawing.Size(75, 23);
+            this.adauga.Size = new System.Drawing.Size(100, 28);
             this.adauga.TabIndex = 11;
             this.adauga.Text = "Adauga";
             this.adauga.UseVisualStyleBackColor = false;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(110, 285);
+            this.textBox1.Location = new System.Drawing.Point(147, 310);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(187, 20);
+            this.textBox1.Size = new System.Drawing.Size(248, 22);
             this.textBox1.TabIndex = 13;
             // 
             // NumarProdusePreparate
@@ -172,9 +187,10 @@
             this.NumarProdusePreparate.AutoSize = true;
             this.NumarProdusePreparate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.NumarProdusePreparate.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.NumarProdusePreparate.Location = new System.Drawing.Point(13, 285);
+            this.NumarProdusePreparate.Location = new System.Drawing.Point(17, 310);
+            this.NumarProdusePreparate.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.NumarProdusePreparate.Name = "NumarProdusePreparate";
-            this.NumarProdusePreparate.Size = new System.Drawing.Size(60, 20);
+            this.NumarProdusePreparate.Size = new System.Drawing.Size(75, 25);
             this.NumarProdusePreparate.TabIndex = 12;
             this.NumarProdusePreparate.Text = "Numar ";
             this.NumarProdusePreparate.Click += new System.EventHandler(this.NumarProdusePreparate_Click);
@@ -184,18 +200,43 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
-            this.label1.Location = new System.Drawing.Point(13, 305);
+            this.label1.Location = new System.Drawing.Point(17, 348);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(140, 20);
+            this.label1.Size = new System.Drawing.Size(171, 25);
             this.label1.TabIndex = 14;
             this.label1.Text = "produse preparate";
             // 
+            // pretBox
+            // 
+            this.pretBox.AutoSize = true;
+            this.pretBox.BackColor = System.Drawing.Color.BurlyWood;
+            this.pretBox.Location = new System.Drawing.Point(224, 394);
+            this.pretBox.Name = "pretBox";
+            this.pretBox.Size = new System.Drawing.Size(168, 20);
+            this.pretBox.TabIndex = 15;
+            this.pretBox.Text = "Am introdus pretul in lei.";
+            this.pretBox.UseVisualStyleBackColor = false;
+            // 
+            // gramajBox
+            // 
+            this.gramajBox.AutoSize = true;
+            this.gramajBox.BackColor = System.Drawing.Color.BurlyWood;
+            this.gramajBox.Location = new System.Drawing.Point(224, 420);
+            this.gramajBox.Name = "gramajBox";
+            this.gramajBox.Size = new System.Drawing.Size(212, 20);
+            this.gramajBox.TabIndex = 16;
+            this.gramajBox.Text = "Am introdus gramajul in grame.";
+            this.gramajBox.UseVisualStyleBackColor = false;
+            // 
             // AdaugaProdus
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(212)))), ((int)(((byte)(191)))));
-            this.ClientSize = new System.Drawing.Size(336, 393);
+            this.ClientSize = new System.Drawing.Size(448, 498);
+            this.Controls.Add(this.gramajBox);
+            this.Controls.Add(this.pretBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.NumarProdusePreparate);
@@ -211,6 +252,7 @@
             this.Controls.Add(this.Pret);
             this.Controls.Add(this.Nume);
             this.Controls.Add(this.NumeProdus);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "AdaugaProdus";
             this.Text = "AdaugaProdus";
             this.ResumeLayout(false);
@@ -235,5 +277,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label NumarProdusePreparate;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox pretBox;
+        private System.Windows.Forms.CheckBox gramajBox;
     }
 }
